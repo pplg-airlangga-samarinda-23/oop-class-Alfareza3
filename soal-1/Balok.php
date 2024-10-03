dc58a9cbe8976d6d395e5e41b2836942de401af3
Dimas_Fahri_Alfareza <dimasfaal@gmail.com>
2024-10-03 11:51:03 +0800

    Tugas Selesai

--- soal-1/Balok.php
+++ soal-1/Balok.php
@@ -2,23 +2,62 @@
 class Balok
 {
     // deklarasikan atribut-atribut kelas Balok
-
+    public $panjang;
+    public $lebar;
+    public $tinggi;
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($panjang, $lebar, $tinggi){
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
+        $this->tinggi = $tinggi;
+    }
+    public function setPanjang($panjang){
+        $this->panjang=$panjang;
+    }
+    public function setLebar($lebar){
+        $this->lebar=$lebar;
+    }
+    public function setTinggi($tinggi){
+        $this->tinggi=$tinggi;
+    }
+
+    public function getPanjang(){
+        return $this->panjang;
+    }
+
+    public function getLebar(){
+        return $this->lebar;
+    }
+    public function getTinggi(){
+        return $this->tinggi;
+    }
+
 
 
     // definisikan metode menghitung volume, luas permukaan, 
-}
+    public function hitungLuas(){
+        return 2 *($this->panjang * $this->lebar + $this->panjang * $this->lebar + $this->lebar * $this->tinggi);
+    }
 
+    public function hitungVolume(){
+        return ($this->panjang * $this->lebar * $this->tinggi);
+    }
+} 
 $balok_1 = new Balok(4, 8, 5);
 $balok_2 = new Balok(3, 10, 12);
 
 // contoh output: Balok
-
+echo "Balok 1 <br>";
+echo "Volume: ". $balok_1->hitungVolume(). "<br>";
+echo "Luas: ". $balok_1->hitungLuas(). "<br>";
 // Balok 1
 // Volume: 160
 // Luas Permukaan: 184
-
+echo "Balok 2 <br>";
+echo "Volume: ". $balok_2->hitungVolume()."<br>";
+echo "Luas: ". $balok_2->hitungLuas();
 // Balok 2
 // Volume: 360
 // Luas Permukaan: 372
+?>
--- soal-2/Siswa.php
+++ soal-2/Siswa.php
@@ -3,22 +3,71 @@
 class Siswa
 {
     // deklarasikan atribut-atribut kelas Siswa
-
+    public $nama;
+    private $nisn;
+    private $alamat;
+    private $jurusan;
+    private $tanggalLahir;
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($nama, $nisn, $alamat, $jurusan, $tanggalLahir){
+        $this->nama = $nama;
+        $this->nisn = $nisn;
+        $this->alamat = $alamat;
+        $this->jurusan = $jurusan;
+        $this->tanggalLahir = $tanggalLahir;
+    }
 
-
+    public function setNama($nama){
+        $this->nama = $nama;
+    }
+    public function setNisn($nisn){
+        $this->nisn = $nisn;
+    }
+    public function setAlamat($alamat){
+        $this->alamat = $alamat;
+    }
+    public function setJurusan($jurusan){
+        $this->jurusan = $jurusan;
+    }
+    public function setTanggallahir($tanggalLahir){
+        $this->tanggalLahir = $tanggalLahir;
+    }
+    public function getNama(){
+        return $this->nama;
+    }
+    public function getNisn(){
+        return $this->nisn;
+    }
+    public function getAlamat(){
+        return $this->alamat;
+    }
+    public function getJurusan(){
+        return $this->jurusan;
+    }
+    public function getTanggallahir(){
+        return $this->tanggalLahir;
+    }
     // buat metode untuk menghitung umur siswa
-    public function hitungUmur() {
-
-
+    public function hitungUmur()
+    {
+        $tanggal_lahir = explode("-", $this->tanggalLahir);
+        $tahun_lahir = $tanggal_lahir[2];
+        $tahun_sekarang = date("Y");
+        $umur = $tahun_sekarang - $tahun_lahir;
         return $umur;
     }
 }
 
-// buat dua buah objek siswa
+// Buat dua buah objek siswa
 $siswa_1 = new Siswa("Bambang", "1234", "Loa Bakung", "PPLG", "27-11-2006");
 $siswa_2 = new Siswa("Bahrun", "1235", "Air Putih", "PPLG", "13-01-2007");
 
-// contoh output:
-// Bambang lebih tua dibandingkan Bahrun.
+// Contoh output:
+if ($siswa_1->hitungUmur() > $siswa_2->hitungUmur()) {
+    echo $siswa_1->getNama() . " lebih tua dibandingkan " . $siswa_2->getNama() . ".";
+} elseif ($siswa_1->hitungUmur() < $siswa_2->hitungUmur()) {
+    echo $siswa_2->getNama() . " lebih tua dibandingkan " . $siswa_1->getNama() . ".";
+} else {
+    echo $siswa_1->getNama() . " dan " . $siswa_2->getNama() . " sama tua.";
+}
